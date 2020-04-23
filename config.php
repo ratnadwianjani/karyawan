95ceb9759452c541f56d94278487d6c8cd0cc177
ratnadwianjani <ratnadwianjani05@gmail.com>
2020-04-23 16:14:58 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?p<?php
+/**
+ * using mysqli_connect for database connection
+ */
+
+$databaseHost = 'localhost';
+$databaseName = 'coba';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+
